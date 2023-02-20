af7b4ccfd6e06929ff192ac412454d6b796accc9
Mforuqdev <alforuq0003@gmail.com>
2023-02-20 09:59:24 +0500

    mobile menu

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,3 @@ document.querySelector('.menuBtn').addEventListener('click', () => {
   document.querySelector('.hamburger').classList.toggle('hidden')
   document.querySelector('.x').classList.toggle('hidden')
 })
-
-// document.querySelector('.menuBtn').addEventListener('click', () => {
-//   document.querySelector('.hamburger').classList.toggle('-translate-x-full')
-//   document.querySelector('.menu').classList.toggle('hidden')
-//   document.querySelector('.x').classList.toggle('hidden')
-// })
-
-// document.querySelector('.overlay').addEventListener('click', () => {
-//   document.querySelector('.menu').classList.toggle('-translate-x-full')
-//   document.querySelector('.hamburger').classList.toggle('hidden')
-//   document.querySelector('.x').classList.toggle('hidden')
-// })
